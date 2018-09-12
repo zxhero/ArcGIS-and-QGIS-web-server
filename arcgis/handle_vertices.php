c43dcc134ed7021ff3897bbf71691fc22fc49e2f
Xu Zhang <zhangxu415@mails.ucas.ac.cn>
2018-09-12 21:02:12 +0800

    Draw polygon then send data to backend, and draw points based on feed back from backend.

--- arcgis/handle_vertices.php
+++ arcgis/handle_vertices.php
@@ -8,6 +8,10 @@ if (isset($_POST['vertices'])) {
 	$myfile = fopen("/home/zxhero/NUS/ArcGIS/vertices_info.txt", "w") or die("Unable to open file!");
 	fwrite($myfile, $str);
 	fclose($myfile);
-	echo "we write $str";
+	$raw_return_dot = array('dot_count' => 2, 'coordinates' => [[103.78121219018385,1.4021936422769343],[103.93879733422659,1.3592907597157984]]);
+	$return_dot = json_encode($raw_return_dot);
+
+	header("Content-type", "application/json");
+	echo $return_dot;
 }
 ?> 
